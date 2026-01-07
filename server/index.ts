c66862a2e85e14d1bb3af30495de87dd633c2344
OnSeChop2Baby Developer <dev@onsechop2baby.com>
2026-01-07 22:03:15 +0100

    MANAGE EMAIL REQUEST

--- server/index.ts
+++ server/index.ts
@@ -131,9 +131,16 @@ app.use((req, res, next) => {
 (async () => {
   await registerRoutes(httpServer, app);
 
-  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
+
+    // If a route already sent a response, never attempt to send again.
+    // This prevents "ERR_HTTP_HEADERS_SENT" crashes.
+    if (res.headersSent) {
+      console.error(err);
+      return next(err);
+    }
 
     res.status(status).json({ message });
     // Don't crash the dev server on handled API errors.
